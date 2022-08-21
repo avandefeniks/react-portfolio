8a092006e2f71227dd1848541e8061cdebfa0fcf
avandefeniks <angel.vandefeniks@gmail.com>
2022-08-20 19:53:13 -0700

    mobile responsiveness changes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,6 +4,9 @@ import { validateEmail } from '../../utils/helpers';
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     // const { name, email, message } = formState;
+    const [email, setEmail] = useState('');
+    const [userName, setName] = useState('');
+    const [message, setMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
     function handleChange(e) {
@@ -21,15 +24,28 @@ function ContactForm() {
         else {
             if (!e.target.value.length) {
                 setErrorMessage(`${e.target.name} is required.`);
-              } else {
+            } else {
                 setErrorMessage('');
-              }
+            }
         }
-        setFormState({...formState, [e.target.name]: e.target.value})
+        setFormState({ ...formState, [e.target.name]: e.target.value })
 
         if (!errorMessage) {
             setFormState({ ...formState, [e.target.name]: e.target.value });
-          }
+        }
+
+        const inputType = e.target.name;
+        const inputValue = e.target.value;
+
+        // Based on the input type, we set the state of either email, username, and password
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        } else if (inputType === 'name') {
+            setName(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+
     }
 
     // console.log(formState);
@@ -37,6 +53,10 @@ function ContactForm() {
     function handleSubmit(e) {
         e.preventDefault();
         console.log(formState);
+        // setFormState({ name: '', email: '', message: '' });
+        setName('');
+        setMessage('');
+        setEmail('');
     }
 
 
@@ -44,7 +64,7 @@ function ContactForm() {
         <section className='contact-form'>
             <h2 id='contact'>Contact me</h2>
             <h6>This form is currently under construction. Please contact me at <a href="mailto:angelj.vandefeniks@gmail.com">
-                            angelj.vandefeniks@gmail.com</a></h6>
+                angelj.vandefeniks@gmail.com</a></h6>
             <form id='contact-form'>
                 <div>
                     <label htmlFor="name">Name:</label>
@@ -56,7 +76,7 @@ function ContactForm() {
                     <label htmlFor="email">Email address:</label>
                 </div>
                 <div>
-                    { <input type="email" /*defaultValue={email}*/ name="email" onBlur={handleChange} /> }
+                    {<input type="email" /*defaultValue={email}*/ name="email" onBlur={handleChange} />}
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
