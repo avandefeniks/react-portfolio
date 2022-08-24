9c710e48e19e2eedb6068098172042fbff999f06
avandefeniks <angel.vandefeniks@gmail.com>
2022-08-23 19:34:52 -0700

    clear form functionality added

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,10 +3,8 @@ import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    // const { name, email, message } = formState;
-    const [email, setEmail] = useState('');
-    const [userName, setName] = useState('');
-    const [message, setMessage] = useState('');
+    const { name, email, message } = formState;
+ 
     const [errorMessage, setErrorMessage] = useState('');
 
     function handleChange(e) {
@@ -32,21 +30,7 @@ function ContactForm() {
 
         if (!errorMessage) {
             setFormState({ ...formState, [e.target.name]: e.target.value });
-        }
-
-        const inputType = e.target.name;
-        const inputValue = e.target.value;
-
-        // Based on the input type, we set the state of either email, username, and password
-        if (inputType === 'email') {
-            setEmail(inputValue);
-        } else if (inputType === 'name') {
-            setName(inputValue);
-        } else {
-            setMessage(inputValue);
-        }
-
-    }
+        }    }
 
     // console.log(formState);
 
@@ -54,9 +38,6 @@ function ContactForm() {
         e.preventDefault();
         console.log(formState);
         // setFormState({ name: '', email: '', message: '' });
-        setName('');
-        setMessage('');
-        setEmail('');
     }
 
 
@@ -70,19 +51,22 @@ function ContactForm() {
                     <label htmlFor="name">Name:</label>
                 </div>
                 <div>
-                    <input type="text" /*defaultValue={name}*/ onBlur={handleChange} name="name" />
+                    <input type="text" defaultValue={name} value={name} onChange={(e) => setFormState({formState, name: e.target.value})} 
+                    onBlur={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
                 </div>
                 <div>
-                    {<input type="email" /*defaultValue={email}*/ name="email" onBlur={handleChange} />}
+                    {<input type="email" defaultValue={email} value={email} name="email" 
+                    onChange={(e) => setFormState({formState, email: e.target.value})} onBlur={handleChange} />}
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
                 </div>
                 <div>
-                    <textarea name="message" /*defaultValue={message}*/ onBlur={handleChange} rows="5" cols='23' />
+                    <textarea name="message" defaultValue={message} value={message} 
+                    onChange={(e) => setFormState({formState, message: e.target.value})} onBlur={handleChange} rows="5" cols='23' />
                 </div>
                 {errorMessage && (
                     <div>
